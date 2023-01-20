8987a303066da9ca604320b4cb95a34c788e32da
Gautam Singh <gautamsingh1@gautamsingh1-mbp.dhcp.thefacebook.com>
2023-01-20 17:40:02 +0000

    sign up page changes

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const menu = document.querySelector("#mobile-menu");
 const menuLinks = document.querySelector(".navbar__menu");
-const navLogo = document.querySelector('#navbar__logo');
+const navLogo = document.querySelector("#navbar__logo");
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
 
 // Display mobile menu
 const mobileMenu = () => {
@@ -11,46 +14,54 @@ const mobileMenu = () => {
 menu.addEventListener("click", mobileMenu);
 
 //Show active menu when scrolling
-const highlightMenu = () =>{
-  const elem = document.querySelector('.highlight');
-  const homeMenu = document.querySelector('#home-page');
-  const aboutMenu = document.querySelector('#about-page');
-  const serviceMenu = document.querySelector('#services-page');
-  let scrollPos = window.scrollY
+const highlightMenu = () => {
+  const elem = document.querySelector(".highlight");
+  const homeMenu = document.querySelector("#home-page");
+  const aboutMenu = document.querySelector("#about-page");
+  const serviceMenu = document.querySelector("#services-page");
+  let scrollPos = window.scrollY;
 
   // adds the "highlight" class to my menu items
-  if(window.innerWidth > 960 && scrollPos < 600){
-    homeMenu.classList.add('highlight');
-    aboutMenu.classList.remove('highlight');
-    return 
-  } else if(window.innerWidth > 960 && scrollPos < 1400){
-    aboutMenu.classList.add('highlight');
-    homeMenu.classList.remove('highlight');
-    serviceMenu.classList.remove('highlight');
-    return
-  } else if(window.innerWidth > 960 && scrollPos < 2345){
-    serviceMenu.classList.add('highlight');
-    aboutMenu.classList.remove('highlight');
-    return
+  if (window.innerWidth > 960 && scrollPos < 600) {
+    homeMenu.classList.add("highlight");
+    aboutMenu.classList.remove("highlight");
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 1400) {
+    aboutMenu.classList.add("highlight");
+    homeMenu.classList.remove("highlight");
+    serviceMenu.classList.remove("highlight");
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 2345) {
+    serviceMenu.classList.add("highlight");
+    aboutMenu.classList.remove("highlight");
+    return;
   }
 
-  if((elem && window.innerWidth < 960 && scrollPos < 600) || elem){
-    elem.classList.remove('highlight')
+  if ((elem && window.innerWidth < 960 && scrollPos < 600) || elem) {
+    elem.classList.remove("highlight");
   }
-}
+};
 
-
-window.addEventListener('scroll', highlightMenu);
-window.addEventListener('click', highlightMenu);
+window.addEventListener("scroll", highlightMenu);
+window.addEventListener("click", highlightMenu);
 
 // Close Mobile Menu when clicking on a menu item
 const hideMobileMenu = () => {
-  const menuBars = document.querySelector('.is-active');
-  if(window.innerWidth <= 768 && menuBars){
-    menu.classList.toggle('is-active');
-    menuLinks.classList.remove('active');
+  const menuBars = document.querySelector(".is-active");
+  if (window.innerWidth <= 768 && menuBars) {
+    menu.classList.toggle("is-active");
+    menuLinks.classList.remove("active");
   }
-}
+};
 
-menuLinks.addEventListener('click', hideMobileMenu)
-navLogo.addEventListener('click', hideMobileMenu)
+menuLinks.addEventListener("click", hideMobileMenu);
+navLogo.addEventListener("click", hideMobileMenu);
+
+// Sign in Page Code
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
